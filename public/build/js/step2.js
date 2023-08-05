594ec6e3fe07a70df9d76a883167c0daf0fe2c92
Jean Carlo Gómez Narváez <gomezjean266@gmail.com>
2023-08-04 21:09:38 -0500

    seventh commit

--- public/build/js/step2.js
+++ public/build/js/step2.js
@@ -106,7 +106,7 @@ function step2HTML() {
 function functionsStep2() {
   const plans = document.querySelectorAll(".membership");
 
-  const url = `${dominio}/20.2-PROYECTO-email/public/assets/images/billing.json`;
+  const url = `${dominio}/data/billing.json`;
   const data = fetch(url)
     .then((result) => result.json())
     .catch((error) => console.log(error));
